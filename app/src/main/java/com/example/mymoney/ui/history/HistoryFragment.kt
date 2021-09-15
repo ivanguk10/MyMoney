1b8766b6b81d5138c4eb9b60342b92282a0dd1b3
ivanguk10 <48579825+ivanguk10@users.noreply.github.com>
2021-09-15 23:06:42 +0300

    Sort by last week demo

--- app/src/main/java/com/example/mymoney/ui/history/HistoryFragment.kt
+++ app/src/main/java/com/example/mymoney/ui/history/HistoryFragment.kt
@@ -14,8 +14,12 @@ import com.example.mymoney.adapters.MoneyAdapter
 import com.example.mymoney.database.entities.MoneyEntity
 import com.example.mymoney.databinding.FragmentHistoryBinding
 import com.example.mymoney.models.ExpenseModel
+import com.example.mymoney.models.MoneyModel
 import com.example.mymoney.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
 @AndroidEntryPoint
 class HistoryFragment : Fragment() {
@@ -26,6 +30,7 @@ class HistoryFragment : Fragment() {
     private lateinit var mainViewModel: MainViewModel
     private lateinit var moneyEntity: MoneyEntity
     private var listOfNames: ArrayList<String> = arrayListOf()
+    private var cardPosition = 0
 
 
     override fun onCreateView(
@@ -40,28 +45,61 @@ class HistoryFragment : Fragment() {
         mainViewModel.readMoney.observe(viewLifecycleOwner, { cards ->
 
             val firstExpenseList = cards.first().money.expenses
-            adapter.setData(firstExpenseList)
+            adapter.setData(sortForLastWeek(firstExpenseList))
+            binding.cardName.text = cards.first().name
 
             cards.forEach {
                 listOfNames.add(it.name)
             }
+
             val arrayAdapter = ArrayAdapter(requireContext(), R.layout.dropdown_card_name, listOfNames)
             binding.autoCompleteTextView.setAdapter(arrayAdapter)
 
             binding.autoCompleteTextView.setOnItemClickListener { adapterView, view, i, l ->
-                val selectedItem = adapterView.getItemAtPosition(i).toString()
-                //val selected = adapterView.selectedItemId.toString()
                 Toast.makeText(requireContext(), i.toString(), Toast.LENGTH_SHORT).show()
-                val expenseList: ArrayList<ExpenseModel>
-                val card = cards[i]
-                expenseList = card.money.expenses
+                val expenseList = cards[i].money.expenses
                 adapter.setData(expenseList)
+                binding.cardName.text = cards[i].name
+                cardPosition = i
+            }
+
+            val arrayList = arrayListOf("For last week", "For last month", "For last year", "For all time")
+            val timeArrayAdapter = ArrayAdapter(requireContext(), R.layout.dropdown_card_name, arrayList)
+            binding.autoCompleteTimeTextView.setAdapter(timeArrayAdapter)
+            binding.autoCompleteTimeTextView.setOnItemClickListener { adapterView, view, i, l ->
+                val expenseList = cards[cardPosition].money.expenses
+                when(i) {
+                    0 -> {
+                        adapter.setData(sortForLastWeek(expenseList))
+                    }
+                }
             }
         })
 
+
         setUpRecyclerView()
 
+        val calendar = Calendar.getInstance()
+        val dateFormat = SimpleDateFormat("dd-MM-yyyy HH:mm")
+        val a = dateFormat.format(calendar.time)
+        //Toast.makeText(requireContext(), a.toString(), Toast.LENGTH_SHORT).show()
+
+
+
         return binding.root
+    }
+
+    private fun sortForLastWeek(listOfExpenses: ArrayList<ExpenseModel>): ArrayList<ExpenseModel> {
+        val calendar = Calendar.getInstance()
+        val simpleDateFormat = SimpleDateFormat("dd-MM-yyyy HH:mm")
+        val newList: ArrayList<ExpenseModel> = arrayListOf()
+        listOfExpenses.forEach {
+            val date = simpleDateFormat.parse(it.date)
+            if (date.date + 7 > calendar.time.date) {
+                newList.add(it)
+            }
+        }
+        return newList
     }
 
     private fun setUpRecyclerView() {
