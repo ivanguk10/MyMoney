f31cfda517a32c5b5dc00a7bbbda6672df4b22ac
ivanguk10 <48579825+ivanguk10@users.noreply.github.com>
2021-09-17 18:11:32 +0300

    apply today, yesterday, month sort

--- app/src/main/java/com/example/mymoney/SortFragment.kt
+++ app/src/main/java/com/example/mymoney/SortFragment.kt
@@ -1,13 +1,12 @@
 package com.example.mymoney
 
-import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Toast
-import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.mymoney.database.entities.MoneyEntity
@@ -16,7 +15,6 @@ import com.example.mymoney.models.ExpenseModel
 import com.example.mymoney.viewmodel.MainViewModel
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import dagger.hilt.android.AndroidEntryPoint
-import java.text.SimpleDateFormat
 import java.time.LocalDate
 import java.time.LocalDate.parse
 import java.time.format.DateTimeFormatter
@@ -56,7 +54,6 @@ class SortFragment : BottomSheetDialogFragment() {
 
             binding.autoCompleteTextView.setOnItemClickListener { adapterView, view, i, l ->
                 Toast.makeText(requireContext(), i.toString(), Toast.LENGTH_SHORT).show()
-                val expenseList = cards[i].money.expenses
                 moneyEntity = cards[i]
             }
         })
@@ -64,27 +61,35 @@ class SortFragment : BottomSheetDialogFragment() {
         binding.today.setOnClickListener {
             sortChoice = 1
             Toast.makeText(requireContext(), sortChoice.toString(), Toast.LENGTH_SHORT).show()
+            selectedItem(sortChoice)
+            sortedList = sortToday(moneyEntity)
         }
 
         binding.yesterday.setOnClickListener {
             sortChoice = 2
             Toast.makeText(requireContext(), sortChoice.toString(), Toast.LENGTH_SHORT).show()
+            selectedItem(sortChoice)
+            sortedList = sortYesterday(moneyEntity)
         }
 
         binding.week.setOnClickListener {
             sortChoice = 3
             Toast.makeText(requireContext(), sortChoice.toString(), Toast.LENGTH_SHORT).show()
             sortedList = sortWeek(moneyEntity)
+            selectedItem(sortChoice)
         }
 
         binding.month.setOnClickListener {
             sortChoice = 4
             Toast.makeText(requireContext(), sortChoice.toString(), Toast.LENGTH_SHORT).show()
+            sortedList = sortMonth(moneyEntity)
+            selectedItem(sortChoice)
         }
 
         binding.year.setOnClickListener {
             sortChoice = 5
             Toast.makeText(requireContext(), sortChoice.toString(), Toast.LENGTH_SHORT).show()
+            selectedItem(sortChoice)
         }
 
         binding.sortButton.setOnClickListener {
@@ -92,10 +97,53 @@ class SortFragment : BottomSheetDialogFragment() {
             findNavController().navigate(action)
         }
 
-
         return binding.root
     }
 
+    private fun selectedItem(choice: Int) {
+        if (choice == 1) {
+            binding.todayTv.setTextColor(ContextCompat.getColor(requireContext(), R.color.darkPurple))
+            binding.todayCheck.visibility = View.VISIBLE
+        }
+        else {
+           binding.todayTv.setTextColor(ContextCompat.getColor(requireContext(), R.color.lightGray))
+           binding.todayCheck.visibility = View.INVISIBLE
+        }
+        if (choice == 2) {
+            binding.yesterdayTv.setTextColor(ContextCompat.getColor(requireContext(), R.color.darkPurple))
+            binding.yesterdayCheck.visibility = View.VISIBLE
+        }
+        else {
+            binding.yesterdayTv.setTextColor(ContextCompat.getColor(requireContext(), R.color.lightGray))
+            binding.yesterdayCheck.visibility = View.INVISIBLE
+        }
+        if (choice == 3) {
+            binding.weekTv.setTextColor(ContextCompat.getColor(requireContext(), R.color.darkPurple))
+            binding.weekCheck.visibility = View.VISIBLE
+        }
+        else {
+            binding.weekTv.setTextColor(ContextCompat.getColor(requireContext(), R.color.lightGray))
+            binding.weekCheck.visibility = View.INVISIBLE
+        }
+        if (choice == 4) {
+            binding.monthTv.setTextColor(ContextCompat.getColor(requireContext(), R.color.darkPurple))
+            binding.monthCheck.visibility = View.VISIBLE
+        }
+        else {
+            binding.monthTv.setTextColor(ContextCompat.getColor(requireContext(), R.color.lightGray))
+            binding.monthCheck.visibility = View.INVISIBLE
+        }
+        if (choice == 5) {
+            binding.yearTv.setTextColor(ContextCompat.getColor(requireContext(), R.color.darkPurple))
+            binding.yearCheck.visibility = View.VISIBLE
+        }
+        else {
+            binding.yearTv.setTextColor(ContextCompat.getColor(requireContext(), R.color.lightGray))
+            binding.yearCheck.visibility = View.INVISIBLE
+        }
+
+
+    }
 
     private fun sortWeek(money: MoneyEntity?): ArrayList<ExpenseModel> {
 
@@ -107,6 +155,47 @@ class SortFragment : BottomSheetDialogFragment() {
             val start = LocalDate.now().with(dayOfWeek, 1)
             val end = start.plusDays(6)
             if (date == start || date == end || date.isAfter(start) && date.isBefore(end)) {
+                newList.add(it)
+            }
+        }
+        return newList
+    }
+
+    private fun sortMonth(money: MoneyEntity?): ArrayList<ExpenseModel> {
+        val newList: ArrayList<ExpenseModel> = arrayListOf()
+        val now = LocalDate.now()
+        val start = now.withDayOfMonth(1)
+        val end = now.withDayOfMonth(now.lengthOfMonth())
+
+        money?.money?.expenses?.forEach {
+            val date = stringToLocaleDate(it.date)
+            if (date == start || date == end || date.isAfter(start) && date.isBefore(end)) {
+                newList.add(it)
+            }
+        }
+        return newList
+    }
+
+    private fun sortToday(money: MoneyEntity?): ArrayList<ExpenseModel> {
+        val newList: ArrayList<ExpenseModel> = arrayListOf()
+        val today = LocalDate.now()
+
+        money?.money?.expenses?.forEach {
+            val date = stringToLocaleDate(it.date)
+            if (date == today) {
+                newList.add(it)
+            }
+        }
+        return newList
+    }
+
+    private fun sortYesterday(money: MoneyEntity?): ArrayList<ExpenseModel> {
+        val newList: ArrayList<ExpenseModel> = arrayListOf()
+        val yesterday = LocalDate.now().minusDays(1)
+
+        money?.money?.expenses?.forEach {
+            val date = stringToLocaleDate(it.date)
+            if (date == yesterday) {
                 newList.add(it)
             }
         }
